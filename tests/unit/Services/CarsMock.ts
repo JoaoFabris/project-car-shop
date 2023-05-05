831ded144de2ff40db8b14bddb38c1adfe7bbd24
Joao Fabris <jvfabris21@gmail.com>
2023-05-05 13:37:52 -0300

    Test unit Car Service

---
+++ tests/unit/Services/CarsMock.ts
@@ -0,0 +1,36 @@
+export const validCar = {
+  model: 'Marea',
+  year: 2002,
+  color: 'Black',
+  status: true,
+  buyValue: 15.990,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+  
+export const carsList = [
+
+  {
+    id: '644fef95769ac51a032aa37e',
+    model: 'Marea',
+    year: 2002,
+    color: 'Black',
+    status: true,
+    buyValue: 15.99,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+  {
+    id: '644fefc6769ac51a032aa386',
+    model: 'Tempra',
+    year: 1995,
+    color: 'Black',
+    status: false,
+    buyValue: 39,
+    doorsQty: 2,
+    seatsQty: 5,
+  },
+
+];
+  
+export default validCar;
